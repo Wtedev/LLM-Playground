b9796633cac3a4bc3047800e7509be5214a40f1a
wtedev <lamaalmushyqih@gmail.com>
2025-09-16 16:30:53 +0300

    requests

--- python/requestslib.py
+++ python/requestslib.py
@@ -1,9 +1,44 @@
 import requests 
 
+# status function
+def status(websitename,response):
+
+    if response == 200:
+        return (f"{websitename} found")
+    elif response == 404:
+        return (f"{websitename} not found")
+    elif response == 503:  
+        return (f"{websitename} service unavailable")
+    elif response == 999:
+        return (f"{websitename} request denied")
+    else:
+        return (f"{websitename} have other issue {response} ")
+
+# parameters 
+params = { 
+    'name': 'aleen',
+    'age': '43'
+}
+
+# data to be sent to the server
+payload = { 
+    'name': 'aleen',
+    'age': '43'
+}
+
 # Make a GET request to the URLs
 response = requests.get('https://www.linkedin.com/in/lama-almushyqih')
-print("Your status code is for LinkedIn account " + str(response.status_code))
+print(status("linkedin",response.status_code))
 
 response = requests.get('https://www.google.com')
-print("Your status code is for Google " + str(response.status_code))
+print(status("google",response.status_code))
+
+
+# specific web service HTTPbin.org
+response = requests.post('http://HTTPbin.org/post', data=payload)
+print(status("HTTPbin",response.status_code))
+
+res_json = response.json()
+del res_json['origin']
+print("content " + str(res_json))
 
